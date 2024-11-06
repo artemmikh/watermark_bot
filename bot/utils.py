dec8ce38b81de0952293fdeb9d7ccf31fd0ceb26
Михайлов Артём <artem.mikh100@gmail.com>
2024-11-06 16:43:59 +0300

    fix: removing unnecessary code

--- bot/utils.py
+++ bot/utils.py
@@ -11,13 +11,3 @@ def send_message(update, context, message):
         logger.debug(f'Сообщение успешно отправлено в Telegram: {message}')
     except Exception as error:
         logger.error(f'Сбой при отправке сообщения в Telegram: {error}')
-
-
-def check_message(message):
-    """Проверяет, соответствует ли текст формату двух чисел через пробел."""
-    return re.match(r'\d{1,15} \d{1,15}', message)
-
-
-def parse_message(message):
-    """Извлекает две группы чисел из сообщения."""
-    return int(message.split(' ')[0]), int(message.split(' ')[1])
