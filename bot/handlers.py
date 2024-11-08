ff03d8d03787837d19e15d91fe5845257e466594
Михайлов Артём <artem.mikh100@gmail.com>
2024-11-08 17:15:48 +0300

    add feature remove temp files

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,5 +1,5 @@
 from bot.logger import logger
-from bot.utils import send_message, add_watermark
+from bot.utils import send_message, add_watermark, remove_images
 
 
 def start_handler(update, context):
@@ -20,10 +20,12 @@ def document_handler(update, context):
     """Обработчик входящих файлов."""
     send_message(update, context, message='ожидайте ⌛️')
     try:
-        watermarked_file_path = add_watermark(update, context)
+        watermarked_file_path, user_file_path = add_watermark(update, context)
         send_message(update, context, file_path=watermarked_file_path)
+        remove_images(watermarked_file_path, user_file_path)
     except Exception as error:
         logger.error(f'Ошибка при добавлении водяного знака: {error}')
         send_message(update, context,
                      'Не удалось обработать изображение. '
                      'Вы можете отправить изображение в форматах PNG или JPEG')
+    remove_images(watermarked_file_path, user_file_path)
--- bot/utils.py
+++ bot/utils.py
@@ -1,6 +1,6 @@
-from PIL import Image
-from PIL import ImageFont
-from PIL import ImageDraw
+import os
+
+from PIL import Image, ImageDraw, ImageFont
 
 from logger import logger
 
@@ -21,17 +21,24 @@ def send_message(update, context, message=None, file_path=None):
 
 
 def download_user_file(update, context):
-    """Загружает в /images изображение присланное пользователем."""
+    """Загружает в /temp изображение присланное пользователем."""
     file = context.bot.get_file(update.message.document.file_id)
-    file_path = 'images/down_image.jpg'
+    file_path = 'temp/down_image.jpg'
     file.download(file_path)
     return file_path
+
+
+def remove_images(user_file_path, watermarked_file_path):
+    if os.path.exists(watermarked_file_path):
+        os.remove(watermarked_file_path)
+    if os.path.exists(user_file_path):
+        os.remove(user_file_path)
 
 
 def add_watermark(update, context):
     """Добавляет водяной знак на изображение и отправляет его обратно в Telegram."""
     user_file_path = download_user_file(update, context)
-    watermarked_file_path = "images/watermarked_image.png"
+    watermarked_file_path = "temp/watermarked_image.png"
 
     with Image.open(user_file_path).convert("RGBA") as base:
         txt = Image.new("RGBA", base.size, (255, 255, 255, 0))
@@ -39,5 +46,5 @@ def add_watermark(update, context):
         d = ImageDraw.Draw(txt)
         d.text((10, 10), "Hello", font=fnt, fill=(255, 255, 255, 64))
         out = Image.alpha_composite(base, txt)
-        out.save('images/watermarked_image.png', 'PNG')
-    return watermarked_file_path
+        out.save('temp/watermarked_image.png', 'PNG')
+    return watermarked_file_path, user_file_path
